b942a5262ce7669ee02b87c990ec976b44b4cbfb
socketwench <tess@deninet.com>
2015-12-12 08:57:07 -0600

    Enabled Simpletest by default in rebuild.sh.

--- .docker/cli/rebuild.sh
+++ .docker/cli/rebuild.sh
@@ -3,11 +3,12 @@
 /root/scripts/init.sh
 
 mkdir /var/www/sites/default
-mkdir /var/www/sites/default/files
+mkdir --parents --mode=777 /var/www/sites/default/files
 chmod -R 777 /var/www/sites/default
+
+drush si --uri=http://web --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y
 
 mkdir /var/www/sites/simpletest
 chmod -R 777 /var/www/sites/simpletest
 
-drush si --uri=http://web --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y
-
+drush --uri=http://web --root=/var/www en -y simpletest
