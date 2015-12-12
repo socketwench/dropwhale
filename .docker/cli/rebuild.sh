b32e413cb053cc5e8fcac8430be3d716f0b9c587
socketwench <tess@deninet.com>
2015-12-12 09:34:10 -0600

    Updated rebuild.sh to create directories recursively, and set permissions.

--- .docker/cli/rebuild.sh
+++ .docker/cli/rebuild.sh
@@ -2,13 +2,8 @@
 
 /root/scripts/init.sh
 
-mkdir /var/www/sites/default
 mkdir --parents --mode=777 /var/www/sites/default/files
-chmod -R 777 /var/www/sites/default
-
 drush si --uri=http://web --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y
 
-mkdir /var/www/sites/simpletest
-chmod -R 777 /var/www/sites/simpletest
-
+mkdir --parents --mode=777 /var/www/sites/simpletest
 drush --uri=http://web --root=/var/www en -y simpletest
