b39c093ff3338f2ad30dbba5d13be4f7ac2fa34a
socketwench <tess.flynn@gmail.com>
2016-04-25 16:53:38 -0500

    Switched to more standardized containers, php7, enabled opcache.

--- .docker/cli/rebuild.sh
+++ .docker/cli/rebuild.sh
@@ -2,12 +2,14 @@
 
 /root/scripts/init.sh
 
-mkdir --parents --mode=777 /var/www/sites/default/files
-drush si --uri=http://web --root=/var/www --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASS}@db/${DB_1_ENV_MYSQL_DB} -y
+mkdir --parents --mode=777 /var/www/html/sites/default/files
+drush si --uri=http://web --root=/var/www/html --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASSWORD}@db/${DB_1_ENV_MYSQL_DATABASE} -y
 
-mkdir --parents --mode=777 /var/www/sites/simpletest
-drush --uri=http://web --root=/var/www en -y simpletest
+mkdir --parents --mode=777 /var/www/html/sites/simpletest
+drush --uri=http://web --root=/var/www/html en -y simpletest
 
 if [ -n "$MODULE_ENABLE" ]; then
-    drush --uri=http://web --root=/var/www en -y ${MODULE_ENABLE};
+    drush --uri=http://web --root=/var/www/html en -y ${MODULE_ENABLE};
 fi
+
+chmod -R 777 /var/www/html/sites/default/files
