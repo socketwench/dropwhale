7f71c91ab4db32a19b3cff407b2595165203eab3
socketwench <tess.flynn@gmail.com>
2016-06-29 13:29:43 -0500

    Issue #7: Print a login link after rebuilding. Allowed setting of site username and password.

--- .docker/cli/rebuild.sh
+++ .docker/cli/rebuild.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+# Set the default username and password if not set in the Compose override.
+DRUPAL_USER=${DRUPAL_USER:-admin}
+DRUPAL_PASSWORD=${DRUPAL_PASSWORD:-password}
+
 /root/scripts/init.sh
 
 cd /var/www/html
 composer install
 
 mkdir --parents --mode=777 /var/www/html/sites/default/files
-drush si --uri=http://web --root=/var/www/html --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASSWORD}@db/${DB_1_ENV_MYSQL_DATABASE} -y
+drush si --uri=http://web --root=/var/www/html \
+      --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASSWORD}@db/${DB_1_ENV_MYSQL_DATABASE} \
+      -y \
+      --account-name=$DRUPAL_USER --account-pass=$DRUPAL_PASSWORD
 
 mkdir --parents --mode=777 /var/www/html/sites/simpletest
 drush --uri=http://web --root=/var/www/html en -y simpletest
@@ -16,3 +23,5 @@ if [ -n "$MODULE_ENABLE" ]; then
 fi
 
 chmod -R 777 /var/www/html/sites/default/files
+
+echo Please login with $DRUPAL_USER/$DRUPAL_PASSWORD
