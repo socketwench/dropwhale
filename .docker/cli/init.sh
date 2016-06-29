8f501787072d9a0aea403c44facf91c5879eafc9
socketwench <tess.flynn@gmail.com>
2016-06-29 12:51:21 -0500

    Issue #8: Fixed hardcoded branch name in build scripts. Added DRUPAL_BRANCH variable.

--- .docker/cli/init.sh
+++ .docker/cli/init.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+DRUPAL_BRANCH=${DRUPAL_BRANCH:-HEAD}
+
 cd /tmp
 
 if [ -d /tmp/drupal ]; then
   cd /tmp/drupal
   git pull
+elif [ "$DRUPAL_BRANCH" != "HEAD" ]; then
+  echo Using branch $DRUPAL_BRANCH
+  git clone --branch $DRUPAL_BRANCH http://git.drupal.org/project/drupal.git
 else
-  git clone --branch 8.0.x http://git.drupal.org/project/drupal.git
+  git clone http://git.drupal.org/project/drupal.git
 fi
 
 cp -r /tmp/drupal/* /var/www/html/
--- .docker/cli/rebuild.sh
+++ .docker/cli/rebuild.sh
@@ -2,6 +2,9 @@
 
 /root/scripts/init.sh
 
+cd /var/www/html
+composer install
+
 mkdir --parents --mode=777 /var/www/html/sites/default/files
 drush si --uri=http://web --root=/var/www/html --db-url=mysql://${DB_1_ENV_MYSQL_USER}:${DB_ENV_MYSQL_PASSWORD}@db/${DB_1_ENV_MYSQL_DATABASE} -y
 
