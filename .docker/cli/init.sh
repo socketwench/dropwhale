557f334f0bfcc1bab4230bf2c386da88ee45f173
socketwench <tess.flynn@gmail.com>
2016-08-14 20:41:05 -0500

    Removed hard-coded 8.0.x branch in init.sh.

--- .docker/cli/init.sh
+++ .docker/cli/init.sh
@@ -6,8 +6,11 @@ if [ -d /tmp/drupal ]; then
   cd /tmp/drupal
   git pull
 else
-  git clone --branch 8.0.x http://git.drupal.org/project/drupal.git
+  git clone http://git.drupal.org/project/drupal.git
 fi
 
 cp -r /tmp/drupal/* /var/www/html/
 cp /tmp/drupal/.htaccess /var/www/html
+
+cd /var/www/html/
+composer install
