f94102ee72d18104b8a3b13dcec7f535ee0715d1
socketwench <tess.flynn@gmail.com>
2017-04-02 14:23:31 -0500

    Added Ansible, Dockstack as a base, PHPMyAdmin and Mailhog.

--- .dropwhale/web/run-tests.sh
+++ .dropwhale/web/run-tests.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
-cd /var/www/html/
+# Clear the cache.
+cd /var/www/html
+drush cr all
 
 # Run the tests.
 if [ -n $MODULE_NAME ] && [ -z $* ]; then
   php core/scripts/run-tests.sh \
       --url http://web \
-      --dburl mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@db/${MYSQL_DATABASE} \
+      --dburl mysql://root:${MYSQL_ROOT_PASSWORD}@db/${MYSQL_DATABASE} \
       --php /usr/bin/php \
       --module $MODULE_NAME
 else
