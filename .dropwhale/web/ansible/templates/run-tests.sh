f750c229c77a4caedd90a1a856b33bfe0b4583c5
socketwench <tess@deninet.com>
2018-11-16 14:37:08 -0600

    Added options to default test run in run-tests.sh.

--- .dropwhale/web/ansible/templates/run-tests.sh
+++ .dropwhale/web/ansible/templates/run-tests.sh
@@ -4,13 +4,22 @@
 cd {{ web_dir }}
 drush cr
 
+# Cleanup from previous Tests
+php core/scripts/run-tests.sh \
+    --url {{ drupal_uri }} \
+    --dburl mysql://root:${MYSQL_ROOT_PASSWORD}@db/${MYSQL_DATABASE} \
+    --php /usr/bin/php \
+    --clean
+
 # Run the tests.
 if [ -n $MODULE_NAME ] && [ -z $* ]; then
   php core/scripts/run-tests.sh \
       --url {{ drupal_uri }} \
       --dburl mysql://root:${MYSQL_ROOT_PASSWORD}@db/${MYSQL_DATABASE} \
       --php /usr/bin/php \
-      --module $MODULE_NAME
+      --module $MODULE_NAME \
+      --verbose \
+      --color
 else
   php core/scripts/run-tests.sh \
       --url {{ drupal_uri }} \
